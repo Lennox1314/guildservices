f6e8076f38a96b63e0f74ec35bde943ab00baa8e
Lennox <114639910+Lennox1314@users.noreply.github.com>
2025-01-14 20:40:36 -0700

    change url

--- callback.php
+++ callback.php
@@ -4,7 +4,7 @@ session_start();
 // Replace with your Discord app details
 $client_id = '1328923275681726525';
 $client_secret = 'QlqPvCElK44zPySdLZZMo6KPpa9X4t8w';
-$redirect_uri = 'http://localhost:80/discord/auth/callback';
+$redirect_uri = 'http://localhost:80/content/guildservices/callback.php';
 $scope = 'identify email'; // Adjust scope as needed
 
 // Check if 'code' is set in the query string
