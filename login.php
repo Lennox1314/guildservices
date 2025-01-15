7e2a22a8205db2269a6508050707d9ccfdd00523
Lennox <114639910+Lennox1314@users.noreply.github.com>
2025-01-14 20:42:23 -0700

    change url to ip

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 // Replace with your Discord app details
 $client_id = '1328923275681726525';
 $client_secret = 'QlqPvCElK44zPySdLZZMo6KPpa9X4t8w';
-$redirect_uri = 'http://localhost:80/content/guildservices/callback.php';
+$redirect_uri = 'http://192.168.0.107:80/content/guildservices/callback.php';
 $scope = 'identify email'; // Adjust scope as needed
 
 $login_url = "https://discord.com/api/oauth2/authorize?client_id=$client_id&redirect_uri=" . urlencode($redirect_uri) . "&response_type=code&scope=" . urlencode($scope);
