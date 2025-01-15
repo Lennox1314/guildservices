b75e44c7a4f62666c649e0dae82d8934e1350f4e
Lennox <114639910+Lennox1314@users.noreply.github.com>
2025-01-14 20:22:18 -0700

    Create login.php

---
+++ login.php
@@ -0,0 +1,13 @@
+<?php
+// Replace with your Discord app details
+$client_id = '1328923275681726525';
+$client_secret = 'QlqPvCElK44zPySdLZZMo6KPpa9X4t8w';
+$redirect_uri = 'http://localhost:80/discord/auth/callback';
+$scope = 'identify email'; // Adjust scope as needed
+
+$login_url = "https://discord.com/api/oauth2/authorize?client_id=$client_id&redirect_uri=" . urlencode($redirect_uri) . "&response_type=code&scope=" . urlencode($scope);
+
+?>
+<a href="<?= $login_url ?>">
+  <button>Login with Discord</button>
+</a>
