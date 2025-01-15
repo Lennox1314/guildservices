e7bd47f48abe2aaacceb2affbb868798d35fe0e8
Lennox <114639910+Lennox1314@users.noreply.github.com>
2025-01-14 21:38:59 -0700

    why broke

--- login.php
+++ login.php
@@ -3,9 +3,8 @@
 $client_id = '1328923275681726525';
 $client_secret = 'QlqPvCElK44zPySdLZZMo6KPpa9X4t8w';
 $redirect_uri = 'http://192.168.0.107:80/guildservices/callback.php';
-
-$login_url = 
 ?>
+
 <a href="init-oauth.php">
   <button>Login with Discord</button>
 </a>
