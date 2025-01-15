8c0b544a355be391dee0a390005cbd154ba55642
Lennox <114639910+Lennox1314@users.noreply.github.com>
2025-01-14 20:57:21 -0700

    changed token url to discord generated one

--- callback.php
+++ callback.php
@@ -17,7 +17,7 @@ $code = $_GET['code'];
 file_put_contents('/content/log.txt', "Authorization Code: $code\n", FILE_APPEND);
 
 // Exchange code for access token
-$token_url = 'https://discord.com/api/oauth2/token';
+$token_url = 'https://discord.com/oauth2/authorize?client_id=1328923275681726525&response_type=code&redirect_uri=http%3A%2F%2F192.168.0.107%3A80%2Fguildservices%2Fcallback.php&scope=identify';
 $data = [
     'client_id' => $client_id,
     'client_secret' => $client_secret,
