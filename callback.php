6963d4c2cab2a8d66309131149467cad608e6c9a
Lennox <114639910+Lennox1314@users.noreply.github.com>
2025-01-14 21:45:50 -0700

    add ids/secret

--- callback.php
+++ callback.php
@@ -1,5 +1,10 @@
 <?php
 
+$client_id = '1328923275681726525';
+$client_secret = 'QlqPvCElK44zPySdLZZMo6KPpa9X4t8w';
+$redirect_uri = 'http://192.168.0.107:80/guildservices/callback.php';
+
+
 if (!isset($_GET['code'])) {
    echo 'couldnt find code';
 }
