9dcaf941ae0e18c839d58d4871df710db2e63dd8
Lennox <114639910+Lennox1314@users.noreply.github.com>
2025-01-14 21:44:52 -0700

    adding curls and payload

--- callback.php
+++ callback.php
@@ -4,7 +4,28 @@ if (!isset($_GET['code'])) {
    echo 'couldnt find code';
 }
 
+$data = [
+    'client_id' => $client_id,
+    'client_secret' => $client_secret,
+    'grant_type' => 'authorization_code',
+    'code' => $code,
+    'redirect_uri' => $redirect_uri,
+    'scope' => 'identify'
+];
 
+print_r($data);
+
+$data_string = http_build_query($data);
+
+$discord_token_url = "https://discordapp.com/api/oauth2/token";
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $token_url);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+curl_close($ch);
 ?>
 
 
